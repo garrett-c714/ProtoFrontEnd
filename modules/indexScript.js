0220c67612281508e0f5bacef21206e36fc72c3c
Garrett Cox <gcox5714@gmail.com>
2020-12-29 21:26:45 -0500

    added scripts to query form

--- modules/indexScript.js
+++ modules/indexScript.js
@@ -1,1 +1,13 @@
+import {Backend} from './backend.js';
 const queryForm = document.querySelector('#query');
+const textfield = document.querySelector('#query-endpoint');
+const landing = document.querySelector('#landing');
+const API = new Backend();
+API.setDefaultUrl('http://127.0.0.1:8000');
+queryForm.addEventListener('submit' , event => {
+    event.preventDefault();
+    API.get(`/${textfield.value}`)
+    .then(data => {
+        landing.textContent = 'uh';
+    });
+});
