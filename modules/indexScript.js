3693d901b3e9c7df289d237233edbfd951b7a1df
Garrett Cox <gcox5714@gmail.com>
2020-12-29 23:53:43 -0500

    added some functionality

--- modules/indexScript.js
+++ modules/indexScript.js
@@ -2,12 +2,17 @@ import {Backend} from './backend.js';
 const queryForm = document.querySelector('#query');
 const textfield = document.querySelector('#query-endpoint');
 const landing = document.querySelector('#landing');
+const title = document.querySelector('h1');
 const API = new Backend();
 API.setDefaultUrl('http://127.0.0.1:8000');
 queryForm.addEventListener('submit' , event => {
     event.preventDefault();
     API.get(`/${textfield.value}`)
     .then(data => {
-        landing.textContent = 'uh';
+        landing.textContent = JSON.stringify(data);
+        console.log(data);
     });
 });
+title.addEventListener('click' , () => {
+    title.classList.toggle('green');
+})
